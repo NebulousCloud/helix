dd3e14b83a3ddfeb40b02ad5136ce45c962f5f4b
Chessnut <chessnutist@gmail.com>
2013-06-28 15:59:40 -0700

    Added spawn saving plugin.

---
+++ plugins/sh_savepos.lua
@@ -0,0 +1,20 @@
+PLUGIN.name = "Save Position"
+PLUGIN.author = "Chessnut"
+PLUGIN.desc = "Saves where a character left."
+
+function PLUGIN:CharacterSave(client)
+	client.character:SetData("pos", client:GetPos())
+	client.character:SetData("posmap", game.GetMap())
+end
+
+function PLUGIN:PlayerSpawn(client)
+	local map = client.character:GetData("posmap")
+	local position = client.character:GetData("pos")
+
+	if (map and map == game.GetMap() and position) then
+		client:SetPos(position + Vector(0, 0, 8))
+	end
+
+	client.character:SetData("posmap", nil)
+	client.character:SetData("pos", nil)
+end
