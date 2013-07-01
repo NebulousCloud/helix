2a9ae55acc25f09ffac03f6f215563e6a416d799
Chessnut <chessnutist@gmail.com>
2013-07-01 11:16:23 -0700

    WIP Stuff

---
+++ gamemode/libs/sh_class.lua
@@ -0,0 +1,7 @@
+--[[
+	Purpose: Provides a library for creating factions and having
+	players able to be whitelisted to certain factions.
+--]]
+
+nut.class = nut.class or {}
+nut.class.buffer = nut.class.buffer or {}
