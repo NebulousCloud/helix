aff3d577fa029fc3110137a8098604e599c09ac3
Chessnut <chessnutist@gmail.com>
2013-07-03 09:17:09 -0700

    Added nutscript.sql for external databases.

---
+++ nutscript.sql
@@ -0,0 +1,25 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+CREATE TABLE `characters` (
+  `key` mediumint(8) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `steamid` bigint(30) unsigned NOT NULL,
+  `charname` varchar(60) NOT NULL,
+  `description` varchar(240) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `money` mediumint(8) unsigned NOT NULL,
+  `inv` mediumtext NOT NULL,
+  `faction` tinyint(4) unsigned NOT NULL,
+  `id` tinyint(4) unsigned NOT NULL,
+  `chardata` mediumtext NOT NULL,
+  `rpschema` varchar(16) NOT NULL,
+  `model` tinytext NOT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `players` (
+  `key` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `steamid` bigint(30) unsigned NOT NULL,
+  `whitelists` varchar(100) NOT NULL,
+  `plydata` mediumtext NOT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
