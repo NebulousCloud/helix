8aaed1c0a0d145d66b9bd984576e5e493b8481c1
Chessnut <chessnutist@gmail.com>
2013-07-28 13:24:36 -0700

    Added pickup ability for fists.

--- entities/weapons/nut_fists.lua
+++ entities/weapons/nut_fists.lua
@@ -9,7 +9,7 @@ if (CLIENT) then
 end
 
 SWEP.Author = "Chessnut"
-SWEP.Instructions = "Primary Fire: [RAISED] Punch,\nSecondary Fire: Knock"
+SWEP.Instructions = "Primary Fire: Throw/[RAISED] Punch,\nSecondary Fire: Knock/Pickup"
 SWEP.Purpose = "Hitting things and knocking on doors."
 SWEP.Drop = false
 
@@ -109,6 +109,10 @@ end
 function SWEP:PrimaryAttack()	
 	self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
 
+	if (IsValid(self.grab)) then
+		self:StopGrab(240)
+	end
+
 	if (!self.Owner:WepRaised()) then
 		return
 	end
@@ -174,27 +178,203 @@ function SWEP:PrimaryAttack()
 	end)
 end
 
+function SWEP:CanCarry(entity, physicsObject)
+	if (physicsObject:GetMass() > 100 or !physicsObject:IsMoveable()) then
+		return false
+	end
+
+	if (IsValid(entity.carrier)) then
+		return false
+	end
+
+	return true
+end
+
+function SWEP:StartGrab(entity, trace)
+	self:SetNextPrimaryFire(CurTime() + 1)
+	self:SetNextSecondaryFire(CurTime() + 1)
+
+	local physicsObject = entity:GetPhysicsObject()
+	local canCarry = false
+
+	if (IsValid(physicsObject)) then
+		canCarry = self:CanCarry(entity, physicsObject)
+	end
+
+	if (!canCarry) then
+		return
+	end
+
+	self.grabbedEntity = entity
+
+	self.grab = ents.Create("prop_physics")
+	self.grab:SetPos(self.Owner:GetShootPos() + self.Owner:GetAimVector() * 54)
+	self.grab:SetModel("models/props_junk/popcan01a.mdl")
+	self.grab:SetNoDraw(true)
+	self.grab:Spawn()
+	self.grab:Activate()
+	self.grab:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
+	self.grab:GetPhysicsObject():EnableMotion(false)
+
+	entity:SetPos(self.grab:GetPos())
+	entity:SetCollisionGroup(COLLISION_GROUP_WEAPON)
+	entity:SetOwner(self.Owner)
+	entity.carrier = self.Owner
+
+	if (IsValid(physicsObject)) then
+		physicsObject:AddGameFlag(FVPHYSICS_PLAYER_HELD)
+	end
+
+	self.grabConstraint = constraint.Weld(self.grab, entity, 0, trace.PhysicsBone, 0, true)
+	self:DeleteOnRemove(self.grab)
+
+	self.Owner:EmitSound("physics/body/body_medium_impact_soft"..math.random(1, 4)..".wav")
+
+	timer.Simple(0.1, function()
+		if (!IsValid(entity)) then
+			return
+		end
+
+		self:CallOnClient("PickUp", entity:EntIndex())
+	end)
+end
+
+function SWEP:Holster(weapon)
+	if (SERVER) then
+		self:StopGrab()
+	end
+
+	return true
+end
+
+function SWEP:StopGrab(force)
+	self:SetNextPrimaryFire(CurTime() + 1)
+	self:SetNextSecondaryFire(CurTime() + 1)
+
+	force = force or 32
+
+	if (IsValid(self.grabConstraint)) then
+		self.grabConstraint:Remove()
+	end
+
+	if (IsValid(self.grab)) then
+		self.grab:Remove()
+	end
+
+	local entity = self.grabbedEntity
+
+	self.Owner:EmitSound("physics/body/body_medium_impact_soft"..math.random(5, 7)..".wav")
+
+	timer.Simple(0.05, function()
+		if (IsValid(entity)) then
+			entity.carrier = nil
+			entity:SetOwner(NULL)
+
+			local velocity = self.Owner:GetAimVector() * force
+			local physicsObject = entity:GetPhysicsObject()
+
+			if (entity:GetClass() == "prop_ragdoll") then
+				for i = 0, entity:GetPhysicsObjectCount() do
+					local physicsObject = entity:GetPhysicsObjectNum(i)
+
+					if (IsValid(physicsObject)) then
+						physicsObject:SetVelocity(velocity)
+					end
+				end
+			elseif (IsValid(physicsObject)) then
+				physicsObject:Wake()
+				physicsObject:EnableMotion(true)
+				physicsObject:SetVelocity(velocity)
+				physicsObject:ClearGameFlag(FVPHYSICS_PLAYER_HELD)
+				physicsObject:AddGameFlag(FVPHYSICS_WAS_THROWN)
+			end
+
+			entity:SetCollisionGroup(COLLISION_GROUP_NONE)
+
+			timer.Simple(0.1, function()
+				if (!IsValid(entity)) then
+					return
+				end
+				print(entity)
+				self:CallOnClient("Dropped", entity:EntIndex())
+			end)
+		end
+	end)
+
+	self.grabbedEntity = nil
+end
+
 function SWEP:SecondaryAttack()
 	local trace = self.Owner:GetEyeTraceNoCursor()
 	local entity = trace.Entity
 
-	if ( SERVER and IsValid(entity) and string.find(entity:GetClass(), "door") ) then
+	if (IsValid(self.grab)) then
+		self:StopGrab()
+
+		return
+	end
+
+	if (SERVER and IsValid(entity)) then
 		local distance = self.Owner:EyePos():Distance(trace.HitPos)
 
 		if (distance > 72) then
 			return
 		end
 
-		if (nut.schema.Call("PlayerCanKnock", self.Owner, entity) == false) then
-			return
+		if (string.find(entity:GetClass(), "door")) then
+			if (nut.schema.Call("PlayerCanKnock", self.Owner, entity) == false) then
+				return
+			end
+
+			self.Owner:ViewPunch( Angle(-1.3, 1.8, 0) )
+			self.Owner:EmitSound("physics/plastic/plastic_box_impact_hard"..math.random(1, 4)..".wav")	
+			self.Owner:SetAnimation(PLAYER_ATTACK1)
+
+			self:DoPunchAnimation()
+			self:SetNextSecondaryFire(CurTime() + 0.4)
+			self:SetNextPrimaryFire(CurTime() + 1)
+		elseif (!entity:IsPlayer() and !entity:IsNPC()) then
+			self:StartGrab(entity, trace)
 		end
+	end
+end
 
-		self.Owner:ViewPunch( Angle(-1.3, 1.8, 0) )
-		self.Owner:EmitSound("physics/plastic/plastic_box_impact_hard"..math.random(1, 4)..".wav")	
-		self.Owner:SetAnimation(PLAYER_ATTACK1)
+function SWEP:Think()
+	local grab = self.grab
+	local grabbedEntity = self.grabbedEntity
 
-		self:DoPunchAnimation()
-		self:SetNextSecondaryFire(CurTime() + 0.4)
-		self:SetNextPrimaryFire(CurTime() + 1)
+	if (SERVER and IsValid(grab) and IsValid(grabbedEntity)) then
+		local data = {
+			start = self.Owner:GetShootPos(),
+			endpos = self.Owner:GetShootPos() + self.Owner:GetAimVector() * 54,
+			filter = {self.Owner, self.grabbedEntity}
+		}
+		local trace = util.TraceLine(data)
+
+		grab:SetPos(trace.HitPos)
+		grabbedEntity:PhysWake()
+	end
+end
+
+function SWEP:PickUp(index)
+	local entity = Entity(tonumber(index))
+
+	if (IsValid(entity) and entity:GetOwner() == LocalPlayer()) then
+		local color = entity:GetColor()
+		color.a = 200
+
+		entity:SetRenderMode(RENDERMODE_TRANSALPHADD)
+		entity:SetColor(color)
+	end
+end
+
+function SWEP:Dropped(index)
+	local entity = Entity(tonumber(index))
+
+	if (IsValid(entity)) then
+		local color = entity:GetColor()
+		color.a = 255
+
+		entity:SetColor(color)
 	end
 end
