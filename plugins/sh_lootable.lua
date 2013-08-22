9c1be8d53b9203b2003cc317f2a9c843cc4aa166
Kyu Yeon Lee <rebel1324@gmail.com>
2013-08-23 03:32:53 +0900

    Fixed Loot Plugin
    ;_;

--- plugins/sh_lootable.lua
+++ plugins/sh_lootable.lua
@@ -20,49 +20,49 @@ PLUGIN.config.fixchance = {
 
 
 function PLUGIN:PlayerDeath( ply, dmg, att )
-	if !self.config.enable then return false
-	local fct = ply:Team()
-	ply:StripAmmo()
-	local entity = ents.Create("nut_container")
-	entity:SetPos( ply:GetPos() + Vector( 0, 0, 10 ) )
-	entity:Spawn()
-	entity:Activate()
-	entity:SetNetVar("inv", {})
-	entity:SetNetVar("name", "Belongings" )
-	entity:SetNetVar( "max", 20 )
-	entity:SetModel( self.config.bagmodel )
-	entity:PhysicsInit(SOLID_VPHYSICS)
-	entity.generated = true
-	local physicsObject = entity:GetPhysicsObject()
-	if (IsValid(physicsObject)) then
-		physicsObject:Wake()
-	end
+	if self.config.enable then
+		local fct = ply:Team()
+		ply:StripAmmo()
+		local entity = ents.Create("nut_container")
+		entity:SetPos( ply:GetPos() + Vector( 0, 0, 10 ) )
+		entity:Spawn()
+		entity:Activate()
+		entity:SetNetVar("inv", {})
+		entity:SetNetVar("name", "Belongings" )
+		entity:SetNetVar( "max", 20 )
+		entity:SetModel( self.config.bagmodel )
+		entity:PhysicsInit(SOLID_VPHYSICS)
+		entity.generated = true
+		local physicsObject = entity:GetPhysicsObject()
+		if (IsValid(physicsObject)) then
+			physicsObject:Wake()
+		end
+				
+		for k,v in pairs( ply:GetInventory() ) do
+			local dice = math.random( 0, 100 )
+			local chance = math.Clamp( self.config.losechance, 0, 100 )
+			if self.config.fixchance[ k ] then
+				local t = type( self.config.fixchance[ k ] )
+				if self.config.fixchance[ k ][ fct ] then
+					chance = math.Clamp( self.config.fixchance[ k ][ fct ], 0, 100 )
+				end
+			end
 			
-	for k,v in pairs( ply:GetInventory() ) do
-		local dice = math.random( 0, 100 )
-		local chance = math.Clamp( self.config.losechance, 0, 100 )
-		if self.config.fixchance[ k ] then
-			local t = type( self.config.fixchance[ k ] )
-			if self.config.fixchance[ k ][ fct ] then
-				chance = math.Clamp( self.config.fixchance[ k ][ fct ], 0, 100 )
+			if dice <= chance then
+				local dat = v.data or {}
+				local q =  1
+				if v.quantity then
+					q = math.random( 1, v.quantity )
+				end
+				ply:UpdateInv( k, -q, dat ) 
+				entity:UpdateInv( k, q, dat ) 
 			end
 		end
 		
-		if dice <= chance then
-			local dat = v.data or {}
-			local q =  1
-			if v.quantity then
-				q = math.random( 1, v.quantity )
+		timer.Simple( self.config.staytime, function()
+			if entity:IsValid() then
+				entity:Remove()
 			end
-			ply:UpdateInv( k, -q, dat ) 
-			entity:UpdateInv( k, q, dat ) 
-		end
+		end)
 	end
-	
-	timer.Simple( self.config.staytime, function()
-		if entity:IsValid() then
-			entity:Remove()
-		end
-	end)
-	
 end
