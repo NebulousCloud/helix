fa81cb64d6dbe5bd812883592e6d5fa0cb14fd3e
Chessnut <chessnutist@gmail.com>
2013-09-02 10:49:04 -0700

    Began working on the panel for moderator.

---
+++ plugins/moderator/cl_derma.lua
@@ -0,0 +1,34 @@
+local PANEL = {}
+	function PANEL:Init()
+		self:SetPos(ScrW() * 0.375, ScrH() * 0.125)
+		self:SetSize(ScrW() * nut.config.menuWidth, ScrH() * nut.config.menuHeight)
+		self:MakePopup()
+		self:SetTitle("Moderator")
+
+		self.list = self:Add("DCategoryList")
+		self.list:Dock(FILL)
+		self.list:SetDrawBackground(true)
+
+		self.catCommands = self.list:Add("Commands")
+		self.catCommands:SetExpanded(false)
+
+		self.catUsers = self.list:Add("Users")
+		self.catUsers:SetExpanded(false)
+
+		self.catBans = self.list:Add("Bans")
+		self.catBans:SetExpanded(false)
+	end
+
+	function PANEL:Think()
+		if (!self:IsActive()) then
+			self:MakePopup()
+		end
+	end
+vgui.Register("nut_Moderator", PANEL, "DFrame")
+
+function PLUGIN:CreateMenuButtons(menu, addButton)
+	addButton("mod", "Moderator", function()
+		nut.gui.inv = vgui.Create("nut_Moderator", menu)
+		menu:SetCurrentMenu(nut.gui.inv)
+	end)
+end
