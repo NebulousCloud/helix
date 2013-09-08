7fc0648ee7ca5e83d5d7d653aea10687657903ca
Kyu Yeon Lee <rebel1324@gmail.com>
2013-09-09 00:26:24 +0900

    Added some examples for nuts.
    I hope you're not a nut.

---
+++ gamemode/items/sh_example_ammo.lua
@@ -0,0 +1,7 @@
+ITEM.name = "Pistol Ammo"
+ITEM.desc = "Box of 30 Bullets for Pistol-type Guns. ( EXAMPLE ITEM )"
+ITEM.base = "base_ammo"
+ITEM.uniqueID = "exampel_ammo"
+ITEM.type = "pistol"
+ITEM.amount = 30
+ITEM.model = Model( "models/Items/BoxSRounds.mdl" )
---
+++ gamemode/items/sh_example_clothing.lua
@@ -0,0 +1,7 @@
+ITEM.name = "Cheaple"
+ITEM.desc = "Makes you more low-res and fancy"
+ITEM.base = "base_cloth"
+ITEM.uniqueID = "example_clothing"
+
+ITEM.model = Model( "models/props_c17/BriefCase001a.mdl" )
+ITEM.outfitmodel = Model( "models/Kleiner.mdl" )
