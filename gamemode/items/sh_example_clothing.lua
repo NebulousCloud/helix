43e89ec810ee6a656decf15ae4f9bb4743a2cdb3
Brian <chessnutist@gmail.com>
2013-09-08 08:56:34 -0700

    Update sh_example_clothing.lua

--- gamemode/items/sh_example_clothing.lua
+++ gamemode/items/sh_example_clothing.lua
@@ -1,7 +1,5 @@
-ITEM.name = "Cheaple"
-ITEM.desc = "Makes you more low-res and fancy"
+ITEM.name = "Kleiner"
+ITEM.desc = "Makes you look like Kleiner!"
 ITEM.base = "base_cloth"
 ITEM.uniqueID = "example_clothing"
-
-ITEM.model = Model( "models/props_c17/BriefCase001a.mdl" )
-ITEM.outfitmodel = Model( "models/Kleiner.mdl" )
+ITEM.model = Model("models/kleiner.mdl")
