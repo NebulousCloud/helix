047f68c4fd0aff78b61386754b5a6d6382d7e70e
Chessnut <chessnutist@gmail.com>
2013-09-08 08:59:35 -0700

    Fixed examples. You only need ITEM.base if it is in the root items folder.

--- gamemode/items/ammo/sh_example.lua
+++ gamemode/items/ammo/sh_example.lua
@@ -1,6 +1,5 @@
 ITEM.name = "Pistol Ammo"
 ITEM.desc = "Box of 30 Bullets for Pistol-type Guns. ( EXAMPLE ITEM )"
-ITEM.base = "base_ammo"
 ITEM.uniqueID = "exampel_ammo"
 ITEM.type = "pistol"
 ITEM.amount = 30
--- gamemode/items/sh_example_clothing.lua
+++
@@ -1,5 +0,0 @@
-ITEM.name = "Kleiner"
-ITEM.desc = "Makes you look like Kleiner!"
-ITEM.base = "base_cloth"
-ITEM.uniqueID = "example_clothing"
-ITEM.model = Model("models/kleiner.mdl")
