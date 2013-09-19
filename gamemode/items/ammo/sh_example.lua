5ce29921c42abf71ba21066366af8601affe30cc
Brian <chessnutist@gmail.com>
2013-09-18 18:59:33 -0700

    Update sh_example.lua

--- gamemode/items/ammo/sh_example.lua
+++ gamemode/items/ammo/sh_example.lua
@@ -1,6 +1,8 @@
+--[[
 ITEM.name = "Pistol Ammo"
 ITEM.desc = "Box of 30 Bullets for Pistol-type Guns. ( EXAMPLE ITEM )"
 ITEM.uniqueID = "exampel_ammo"
 ITEM.type = "pistol"
 ITEM.amount = 30
 ITEM.model = Model( "models/Items/BoxSRounds.mdl" )
+--]]
