9a9f40146da612db36fc857f95120f3b7554fc8a
Chessnut <chessnutist@gmail.com>
2013-09-18 19:02:21 -0700

    Commented examples.

--- gamemode/items/weapon/sh_example.lua
+++ gamemode/items/weapon/sh_example.lua
@@ -1,6 +1,8 @@
+--[[
 ITEM.name = "Crowbar"
 ITEM.uniqueID = "example_weapon"
 ITEM.class = "weapon_crowbar"
 ITEM.type = "melee"
 ITEM.model = Model( "models/weapons/w_crowbar.mdl" )
 ITEM.desc = "A Crowbar ( EXAMPLE ITEM )"
+--]]
