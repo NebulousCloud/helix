d0859c39cc31c84be5d286f1c22d441f1fccee2f
Chessnut <chessnutist@gmail.com>
2013-10-26 12:19:44 -0700

    Cleaned up .sql file

--- nutscript.sql
+++ nutscript.sql
@@ -1,23 +1,5 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost_3306
-Source Server Version : 50528
-Source Host           : localhost:3306
-Source Database       : nutscript
-
-Target Server Type    : MYSQL
-Target Server Version : 50528
-File Encoding         : 65001
-
-Date: 2013-09-30 18:33:50
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
--- ----------------------------
--- Table structure for `characters`
--- ----------------------------
-DROP TABLE IF EXISTS `characters`;
+
 CREATE TABLE `characters` (
   `key` mediumint(8) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `steamid` bigint(30) unsigned NOT NULL,
@@ -32,16 +14,8 @@ CREATE TABLE `characters` (
   `rpschema` varchar(16) NOT NULL,
   `model` tinytext NOT NULL,
   PRIMARY KEY (`key`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
+) CHARSET=latin1;
 
--- ----------------------------
--- Records of characters
--- ----------------------------
-
--- ----------------------------
--- Table structure for `players`
--- ----------------------------
-DROP TABLE IF EXISTS `players`;
 CREATE TABLE `players` (
   `key` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `steamid` bigint(30) unsigned NOT NULL,
@@ -49,8 +23,4 @@ CREATE TABLE `players` (
   `plydata` mediumtext NOT NULL,
   `rpschema` varchar(16) NOT NULL,
   PRIMARY KEY (`key`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of players
--- ----------------------------
+) CHARSET=latin1;
