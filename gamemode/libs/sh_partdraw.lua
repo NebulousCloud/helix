25cca05ff817b287d6573921886d192051ea0d8b
Kyu Yeon Lee <rebel1324@gmail.com>
2014-02-11 08:17:45 +0900

    added comment in partdraw.
    yes

--- gamemode/libs/sh_partdraw.lua
+++ gamemode/libs/sh_partdraw.lua
@@ -1,11 +1,32 @@
+--[[
+	Purpose: Draws parts for players.
+	Created by rebel1324(Black Tea). If you have any questions about PartDraw Library, ask to me.
+	Do not ask about this to Chessnut.
+--]]
 local playerMeta = FindMetaTable("Player")
+
 if SERVER then
+
+	--[[
+		Purpose: Handles for Adding/Removing parts.
+		
+		PartsTable: A Table saved in player as Nutscript's NetVar.
+		PartData: A Data for Drawing parts on the player.
+
+		player:ResetPartModels(): Resets player PartsTable. returns table.
+		player:GetPartModels(): Get player PartsTable. returns table.
+		player:AddPartModel(UniqueID, DrawData): Add a part with DrawData in PartsTable as 'UniqueID' index.
+		player:RemovePartModel(UniqueID): Remove a part that has 'UniqueID' index in PartsTable.
+		player:HasPartModel(UniqueID): Gets 'UniqueID' index in PartsTable. returns PartData.
+	--]]
 	function playerMeta:ResetPartModels()
 		return self:SetNetVar( "parts", {} )
 	end
+
 	function playerMeta:GetPartModels()
 		return self:GetNetVar( "parts", {} )
 	end
+
 	function playerMeta:AddPartModel(uid, data)
 		local part = self:GetPartModels()
 		if (type(data) != "table") then
@@ -15,6 +36,7 @@ if SERVER then
 		part[uid] = data
 		self:SetNetVar("parts", part)
 	end
+
 	function playerMeta:RemovePartModel(uid)
 		local part = self:GetPartModels()
 		if (part[uid]) then
@@ -22,35 +44,55 @@ if SERVER then
 		end
 		self:SetNetVar("parts", part)
 	end
+
 	function playerMeta:HasPartModel(uid)
 		return self:GetPartModels()[uid]
 	end
+
 else
+
+	--[[
+		Purpose: Handles for Adding/Removing parts.
+		
+		PartsTable: A Table saved in player as Nutscript's NetVar.
+		PartData: A Data for Drawing parts on the player.
+
+		player:ResetPartModels(): Resets player PartsTable. returns table.
+		player:GetPartModels(): Get player PartsTable. returns table.
+		player:AddPartModel(UniqueID, DrawData): Add a part with DrawData in PartsTable as 'UniqueID' index.
+		player:RemovePartModel(UniqueID): Remove a part that has 'UniqueID' index in PartsTable.
+		player:HasPartModel(UniqueID): Gets 'UniqueID' index in PartsTable. returns PartData.
+	--]]
+
 	function playerMeta:GetPartModels()
 		return self:GetNetVar( "parts", {} )
 	end
+
 	local function CanDrawParts(client)
 		return (client:IsValid() && client.character)
 	end
-	-- DRAWCODE MUST BE IMPROVED.
-	-- AWWW SHIT
-	-- I'm such a bad coder.
+
+	--[[
+		Purpose: DrawPlayerParts works pretty well, but I think no one will like this draw parts code.
+	--]]
+
 	function DrawPlayerParts(client)
 		client.drawing = client.drawing or {}
 		if (!CanDrawParts(client)) then return end
 
-		-- For generic parts
+		-- Generic Parts Drawing
 		local models = client:GetPartModels()
 		for k, v in pairs(models) do
-			if (!client.drawing[k]) then -- If clientside part model is not exists.
-				-- holla holla create model.
+			if (!client.drawing[k]) then
 				client.drawing[k] = ClientsideModel( v.model, RENDERGROUP_BOTH )
 				client.drawing[k]:SetNoDraw( true )
 				client.drawing[k]:SetSkin( v.skin or 0 )
 				client.drawing[k]:SetColor( v.color or color_white )
 				client.drawing[k]:ManipulateBoneScale( 0, v.scale )
+
 				if (v.scale) then
 					local matrix = Matrix()
+
 					matrix:Scale( (v.scale or Vector( 1, 1, 1 ))*(v.size or 1) )
 					client.drawing[k]:EnableMatrix("RenderMultiply", matrix)
 				end
@@ -58,15 +100,16 @@ else
 					client.drawing[k]:SetMaterial( v.material )
 				end
 			else
-				-- update position and status.
 				if (client:LookupBone(v.bone)) then
 					local pos, ang = client:GetBonePosition(client:LookupBone(v.bone))
 					local drawingmodel = client.drawing[k] -- localize
 					pos = pos + ang:Forward()*v.position.x + ang:Up()*v.position.z + ang:Right()*-v.position.y
 					local ang2 = ang
-					ang2:RotateAroundAxis( ang:Right(), v.angle.pitch ) -- pitch
-					ang2:RotateAroundAxis( ang:Up(),  v.angle.yaw )-- yaw
-					ang2:RotateAroundAxis( ang:Forward(), v.angle.roll )-- roll
+
+					ang2:RotateAroundAxis( ang:Right(), v.angle.pitch )
+					ang2:RotateAroundAxis( ang:Up(),  v.angle.yaw )
+					ang2:RotateAroundAxis( ang:Forward(), v.angle.roll )
+
 					drawingmodel:SetRenderOrigin( pos )
 					drawingmodel:SetRenderAngles( ang )
 					drawingmodel:DrawModel()
@@ -74,8 +117,7 @@ else
 			end
 		end
 
-		-- For Weapons( eww.. look at this code. )
-		-- temp fix.
+		-- Weapon Holster Drawing.
 		for _, wep in pairs(client:GetWeapons()) do
 			local class = wep:GetClass()
 			local itemTable = nut.item.Get(class)
@@ -83,15 +125,16 @@ else
 				local drawdat = itemTable.wep_partdata
 				local uid = "w_"..class
 				if (client:GetActiveWeapon():GetClass() != class) then
-					if (!client.drawing[uid]) then -- If clientside part model is not exists.
-						-- holla holla create model.
+					if (!client.drawing[uid]) then 
 						client.drawing[uid] = ClientsideModel( drawdat.model, RENDERGROUP_BOTH )
 						client.drawing[uid]:SetNoDraw( true )
 						client.drawing[uid]:SetSkin( drawdat.skin or 0 )
 						client.drawing[uid]:SetColor( drawdat.color or color_white )
 						client.drawing[uid]:ManipulateBoneScale( 0, drawdat.scale )
+						
 						if (drawdat.scale) then
 							local matrix = Matrix()
+
 							matrix:Scale( (drawdat.scale or Vector( 1, 1, 1 ))*(drawdat.size or 1) )
 							client.drawing[uid]:EnableMatrix("RenderMultiply", matrix)
 						end
@@ -99,22 +142,23 @@ else
 							client.drawing[uid]:SetMaterial( drawdat.material )
 						end
 					else
-						-- update position and status.
 						if (client:LookupBone(drawdat.bone)) then
 							local pos, ang = client:GetBonePosition( client:LookupBone( drawdat.bone ) )
 							local drawingmodel = client.drawing[uid] -- localize
 							pos = pos + ang:Forward()*drawdat.position.x + ang:Up()*drawdat.position.z + ang:Right()*-drawdat.position.y
 							local ang2 = ang
-							ang2:RotateAroundAxis( ang:Right(), drawdat.angle.pitch ) -- pitch
-							ang2:RotateAroundAxis( ang:Up(),  drawdat.angle.yaw )-- yaw
-							ang2:RotateAroundAxis( ang:Forward(), drawdat.angle.roll )-- roll
+
+							ang2:RotateAroundAxis( ang:Right(), drawdat.angle.pitch )
+							ang2:RotateAroundAxis( ang:Up(),  drawdat.angle.yaw )
+							ang2:RotateAroundAxis( ang:Forward(), drawdat.angle.roll )
+
 							drawingmodel:SetRenderOrigin( pos )
 							drawingmodel:SetRenderAngles( ang )
 							drawingmodel:DrawModel()
 						end
 					end
 				else
-					if (client.drawing[uid]) then -- If clientside part model is exists.
+					if (client.drawing[uid]) then 
 						client.drawing[uid]:Remove()
 						client.drawing[uid] = nil
 					end
