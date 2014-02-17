c6733e55dc485e40e31ebbc08dc94d003016e5c9
Chessnut <chessnutist@gmail.com>
2014-02-17 10:48:20 -0800

    Neatened the code to conform to coding guidelines.

--- gamemode/derma/cl_settings.lua
+++ gamemode/derma/cl_settings.lua
@@ -10,81 +10,82 @@ local PANEL = {}
 		self.list:SetDrawBackground(true)
 		self.list:DockPadding(10, 10, 10, 10)
 
-		local notice = self:Add( "nut_NoticePanel" )
-		notice:Dock( TOP )
-		notice:DockMargin( 0, 0, 0, 5 )
-		notice:SetType( 4 )
+		local notice = self:Add("nut_NoticePanel")
+		notice:Dock(TOP)
+		notice:DockMargin(0, 0, 0, 5)
+		notice:SetType(4)
 		notice:SetText(nut.lang.Get("settings_tip"))
 
 		self.category = {}
 		self.options = {}
 
-
 		hook.Run("AddSettingOptions", self)
 	end
 
 	function PANEL:AddCategory(name)
-		local cat = self.list:Add("DCollapsibleCategory")
-		cat:Dock(TOP)
-		cat:SetLabel(name)
-		cat:SetExpanded(true)
-		cat:DockMargin(5, 5, 5, 5)
+		local category = self.list:Add("DCollapsibleCategory")
+		category:Dock(TOP)
+		category:SetLabel(name)
+		category:SetExpanded(true)
+		category:DockMargin(5, 5, 5, 5)
 
 		local list = vgui.Create("DPanelList")
 		list:SetSpacing(5)
 		list:SetAutoSize(true)
 		list:EnableVerticalScrollbar(true)
-		--list:SetDrawBackground(true)
-		cat:SetContents(list)
+		
+		category:SetContents(list)
+		category:InvalidateLayout(true)
+		category.list = list
 
-		cat:InvalidateLayout(true)
-		cat.list = list
+		self.category[#self.category] = category
 
-		self.category[#self.category] = cat
-		return cat
+		return category
 	end
 	
 	function PANEL:AddSlider(category, name, min, max, var, demc)
-		if (!category or !name or !min or !max or !var ) then
-
+		if (!category or !name or !min or !max or !var) then
 			return
 		end
 
-		local slder = vgui.Create("DNumSlider")
-		slder:Dock(TOP)
-		slder:SetText(name)
-		slder.Label:SetTextColor(Color(22, 22, 22))
-		slder:SetMin( min )                
-		slder:SetMax( max )
-		slder:SetDecimals( demc or 0 )
-		slder:SetValue(nut.config[var])
-		slder:DockMargin( 10, 2, 0, 2 )
+		local slider = vgui.Create("DNumSlider")
+		slider:Dock(TOP)
+		slider:SetText(name)
+		slider.Label:SetTextColor(Color(22, 22, 22))
+		slider:SetMin(min)                
+		slider:SetMax(max)
+		slider:SetDecimals(demc or 0)
+		slider:SetValue(nut.config[var])
+		slider:DockMargin(10, 2, 0, 2)
 
-		function slder:OnValueChanged( val )
-			local val = self:GetValue()
-			nut.config[var] = val
+		function slider:OnValueChanged(value)
+			local value = self:GetValue()
+			nut.config[var] = value
 		end
-		category.list:AddItem( slder )
 
-		self.options[#self.options] = slder
+		category.list:AddItem(slider)
+
+		self.options[#self.options] = slider
 	end
 	
-	function PANEL:AddChecker( category, name, var )
+	function PANEL:AddChecker(category, name, var)
 		if (!category or !name or !var) then
-
 			return
 		end
+
 		local checker = vgui.Create("DCheckBoxLabel")
 		checker:Dock(TOP)
 		checker:SetText(name)
 		checker:SetTextColor(Color(22, 22, 22))
 		checker:SetValue((nut.config[var] == true) and 1 or 0)
-		checker:DockMargin( 10, 5, 0, 0 )
+		checker:DockMargin(10, 5, 0, 0)
 		checker:SetTall(20)
-		function checker:OnChange(val)
-			nut.config[var] = val
+
+		function checker:OnChange(value)
+			nut.config[var] = value
 		end
-		category.list:AddItem( checker )
+
+		category.list:AddItem(checker)
 
 		self.options[#self.options] = checker
 	end
