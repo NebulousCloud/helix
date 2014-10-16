2c6cbebb38b64a136d4fcf017686d4b0b1148d58
Kyu Yeon Lee <rebel1324@gmail.com>
2014-10-17 02:19:07 +0900

    Area Plugin is now working.

--- plugins/area/sh_plugin.lua
+++ plugins/area/sh_plugin.lua
@@ -2,44 +2,53 @@ local PLUGIN = PLUGIN
 PLUGIN.name = "Area"
 PLUGIN.author = "Black Tea"
 PLUGIN.desc = "Allows you to set area."
-PLUGIN.areaTable = {}
+PLUGIN.areaTable = PLUGIN.areaTable or {}
 nut.area = nut.area or {}
 ALWAYS_RAISED["nut_areahelper"] = true
 
 local playerMeta = FindMetaTable("Player")
 
-function nut.area.getArea(areaID)
-	return PLUGIN.areaTable[areaID]
-end
-
-function nut.area.getAllArea()
-	return PLUGIN.areaTable
-end
-
--- This is for single check (ex: area items, checking area in commands)
-function playerMeta:isInArea(areaID)
-	local areaData = nut.area.getArea(areaID)
-
-	if (!areaData) then
-		return false, "Area you specified is not valid."
-	end
-
-	local char = v:getChar()
-
-	if (!char) then
-		return false, "Your character is not valid."
-	end
-
-	local clientPos = self:GetPos() + self:OBBCenter()
-	return clientPos:WithinAABox(areaData.minVector, areaData.maxVector), areaData
-end
-
--- This is for continous check (ex: checking gas area whatever.)
-function playerMeta:getArea()
-	return self.curArea
-end
 
 if (SERVER) then
+	function nut.area.getArea(areaID)
+		return PLUGIN.areaTable[areaID]
+	end
+
+	function nut.area.getAllArea()
+		return PLUGIN.areaTable
+	end
+
+	-- This is for single check (ex: area items, checking area in commands)
+	function playerMeta:isInArea(areaID)
+		local areaData = nut.area.getArea(areaID)
+
+		if (!areaData) then
+			return false, "Area you specified is not valid."
+		end
+
+		local char = v:getChar()
+
+		if (!char) then
+			return false, "Your character is not valid."
+		end
+
+		local clientPos = self:GetPos() + self:OBBCenter()
+		return clientPos:WithinAABox(areaData.minVector, areaData.maxVector), areaData
+	end
+
+	-- This is for continous check (ex: checking gas area whatever.)
+	function playerMeta:getArea()
+		return self.curArea
+	end
+
+	function PLUGIN:SaveDota()
+		self:setData(self.areaTable)
+	end
+
+	function PLUGIN:LoadData()
+		self.areaTable = self:getData() or {}
+	end
+
 	local function sortVector(vector1, vector2)
 		local minVector = Vector(0, 0, 0)
 		local maxVector = Vector(0, 0, 0)
@@ -72,16 +81,43 @@ if (SERVER) then
 		})
 	end
 
-	function PLUGIN:PlayerInitialSpawn(client)
-		netstream.Start("areaReceive", nut.area.getAllArea())
+	function PLUGIN:Think()
+		for k, v in ipairs(player.GetAll()) do
+			local char = v:getChar()
+
+			if (char) then
+				local area = v:getArea()
+				for id, areaData in pairs(nut.area.getAllArea()) do
+					local clientPos = v:GetPos() + v:OBBCenter()
+
+					if (clientPos:WithinAABox(areaData.minVector, areaData.maxVector)) then
+						if (area != id) then
+							v.curArea = id
+
+							hook.Run("OnPlayerAreaChanged", v, id)
+						end
+					end
+				end
+			end
+		end
+	end
+
+	function PLUGIN:OnPlayerAreaChanged(client, areaID)
+		local areaData = nut.area.getArea(areaID)
+		netstream.Start(client, "displayAreaText", tostring(areaData.name))
 	end
 else
-	netstream.Hook("areaReceive", function(areaData)
-		if (areaData) then
-			PLUGIN.areaTable = areaData
-		else
-			print("Bad Data Received?")
-		end
+	netstream.Hook("displayPosition", function(pos)
+		local emitter = ParticleEmitter( pos )
+		local bling = emitter:Add( "sprites/glow04_noz", pos )
+		bling:SetVelocity( Vector( 0, 0, 1 ) )
+		bling:SetDieTime(10)
+		bling:SetStartAlpha(255)
+		bling:SetEndAlpha(255)
+		bling:SetStartSize(64)
+		bling:SetEndSize(64)
+		bling:SetColor(255,186,50)
+		bling:SetAirResistance(300)
 	end)
 
 	-- area Manager.
@@ -92,21 +128,199 @@ else
 	netstream.Hook("areaManager", function()
 		nut.area.openAreaManager()
 	end)
-end
 
-function PLUGIN:Think()
-	for k, v in ipairs(player.GetAll()) do
-		local char = v:getChar()
+	function PLUGIN:LoadFonts(font)
+		surface.CreateFont("nutAreaDisplay", {
+			font = font,
+			size = ScreenScale(26),
+			weight = 500,
+			shadow = true,
+		})
+	end
 
-		if (char) then
-			local area = v:getArea()
-			for id, areaData in pairs(nut.area.getAllArea()) do
-				local clientPos = v:GetPos() + v:OBBCenter()
+	-- draw matrix string.
+	-- slow as fuck I guess?
+	local function drawMatrixString(str, font, x, y, scale, angle, color)
+		surface.SetFont(font)
+		local tx, ty = surface.GetTextSize(str)
 
-				if (clientPos:WithinAABox(areaData.minVector, areaData.maxVector)) then
-					v.curArea = id
+		local matrix = Matrix()
+		matrix:Translate(Vector(x, y, 1))
+		matrix:Rotate(angle or Angle(0, 0, 0))
+		matrix:Scale(scale)
+
+		cam.PushModelMatrix(matrix)
+			surface.SetTextPos(2, 2)
+			surface.SetTextColor(color or color_white)
+			surface.DrawText(str)
+		cam.PopModelMatrix()
+	end
+
+	-- fTime(): Powerful Fix for High Framerate Slowness.
+	local function fTime() return math.Clamp(FrameTime(), 1/60, 1) end
+
+	-- configureable values.
+	local speed = 0
+	local targetScale = 0
+	local dispString = ""
+	local tickSound = "UI/buttonrollover.wav"
+	local dieTime = 5
+
+	-- non configureable values.
+	--local scale = 0
+	local scale = 0
+	local flipTable = {}
+	local powTime = RealTime()*speed
+	local curChar = 0
+	local mathsin = math.sin
+	local mathcos = math.cos
+	local dieTrigger = false
+	local dieTimer = RealTime()
+	local dieAlpha = 0
+
+	function displayScrText(str, time)
+		speed = 20
+		targetScale = 1
+		dispString = str
+		dieTime = time or 5
+
+		scale = targetScale * .5
+		flipTable = {}
+		powTime = RealTime()*speed
+		curChar = 0
+		dieTrigger = false
+		dieTimer = RealTime()
+		dieAlpha = 255
+	end
+
+	netstream.Hook("displayAreaText", function(str)
+		displayScrText(str)
+	end)
+
+	function PLUGIN:HUDPaint()
+		-- values
+		if (dieTrigger and dieTimer < RealTime() and dieAlpha <= 1) then
+			return	
+		end
+
+		local w, h = ScrW(), ScrH()
+		local dsx, dsy = 0
+		local strEnd = string.len(dispString)
+		local sx, sy = surface.GetTextSize(dispString)
+		local rTime = RealTime()
+
+		-- Number of characters to display.
+		local maxDisplay = math.Round(rTime*speed - powTime)
+
+		-- resize if it's too big.
+		while (sx and sx*targetScale > w*.8) do
+			targetScale = targetScale * .9
+		end
+
+		-- scale lerp
+		scale = Lerp(fTime()*1, scale, targetScale)
+		--scale = targetScale
+
+		-- change event
+		if (maxDisplay != curChar and curChar < strEnd) then
+			curChar = maxDisplay
+			if (dispString[curChar] != " ") then
+				LocalPlayer():EmitSound(tickSound, 100, math.random(190, 200))
+			end
+		end
+
+		-- draw recursive
+		for i = 1, math.min(maxDisplay, strEnd) do
+			-- character scale/color lerp
+			flipTable[i] = flipTable[i] or {}
+			flipTable[i][1] = flipTable[i][1] or 2
+			--flipTable[i][1] = flipTable[i][1] or targetScale*3
+			flipTable[i][2] = flipTable[i][2] or 0
+			flipTable[i][1] = Lerp(fTime()*4, flipTable[i][1], scale)
+			flipTable[i][2] = Lerp(fTime()*2, flipTable[i][2], 255)
+
+			-- draw character.
+			local tx, ty = surface.GetTextSize(dispString[i])
+			drawMatrixString(dispString[i],
+				"titleShit",
+				math.Round(w/2 + dsx - (sx or 0)*scale/2),
+				math.Round(h/3*1 - (sy or 0)*scale/2),
+				Vector(flipTable[i][1], Format("%.2f", scale), 1),
+				nil,
+				Color(255, 255, 255,
+				(dieTrigger and dieTimer < RealTime()) and dieAlpha or flipTable[i][2])
+			)
+
+			-- next 
+			dsx = dsx + tx*scale
+		end
+
+		if (maxDisplay >= strEnd) then
+			if (dieTrigger != true) then
+				dieTrigger = true
+				dieTimer = RealTime() + 2
+			else
+				if (dieTimer < RealTime()) then
+					dieAlpha = Lerp(fTime()*2, dieAlpha, 0)
 				end
 			end
 		end
 	end
 end
+
+nut.command.add("areaadd", {
+	adminOnly = true,
+	syntax = "<string name> [bool showTime]",
+	onRun = function(client, arguments)
+		local name = arguments[1] or "Area"
+		local showTime = util.tobool(arguments[2] or "true")
+
+		if (!client:getNetVar("areaMin")) then
+			if (!name) then
+				nut.util.Notify(nut.lang.Get("missing_arg", 1), client)
+
+				return
+			end
+
+			local pos = client:GetEyeTraceNoCursor().HitPos
+
+			netstream.Start(client, "displayPosition", pos)
+
+			client:setNetVar("areaMin", pos, client)
+			client:setNetVar("areaName", name, client)
+
+			client:notify("Run the command again at a different position to set a maximum point.")
+		else
+			local data = {}
+			local pos = client:GetEyeTraceNoCursor().HitPos
+			local min = client:getNetVar("areaMin")
+			local max = pos
+			local name = client:getNetVar("areaName")
+
+			netstream.Start(client, "displayPosition", pos)
+
+			client:setNetVar("areaMin", nil, client)
+			client:setNetVar("areaName", nil, client)
+
+			nut.area.addArea(name, min, max)
+			client:notify("You've added a new area.")
+		end
+	end
+})
+
+nut.command.add("arearemove", {
+	adminOnly = true,
+	onRun = function(client, arguments)
+		local count = 0
+
+		for k, v in pairs(PLUGIN.areas) do
+			if (table.HasValue(ents.FindInBox(v.min, v.max), client)) then
+				table.remove(PLUGIN.areas, k)
+
+				count = count + 1
+			end
+		end
+
+		nut.util.Notify("You've removed "..count.." areas.", client)
+	end
+})
