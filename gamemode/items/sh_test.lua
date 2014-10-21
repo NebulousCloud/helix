8c805df1348007f7aa72c8c1ad91ae49aec9fe7a
Chessnut <chessnutist@gmail.com>
2014-10-20 18:03:57 -0700

    Added needed license information to each file

--- gamemode/items/sh_test.lua
+++ gamemode/items/sh_test.lua
@@ -1,3 +1,18 @@
+--[[
+    NutScript is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    NutScript is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NutScript.  If not, see <http://www.gnu.org/licenses/>.
+--]]
+
 ITEM.name = "Test Item"
 ITEM.desc = "A test item!"
 ITEM.model = "models/props_c17/oildrum001.mdl"
