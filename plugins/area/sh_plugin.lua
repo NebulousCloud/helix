5e915ce93d5d06ef59fa3eb6b8275a27b9d1261d
Kyu Yeon Lee <rebel1324@gmail.com>
2014-10-23 17:42:53 +0900

    Added Area Display Surpress

--- plugins/area/sh_plugin.lua
+++ plugins/area/sh_plugin.lua
@@ -21,6 +21,9 @@ PLUGIN.areaTable = PLUGIN.areaTable or {}
 nut.area = nut.area or {}
 ALWAYS_RAISED["nut_areahelper"] = true
 
+-- If area is being very annoying for your plugin/schema, you can surpress it by toggling this variable.
+SURPRESS_AREADISPLAY = false
+
 nut.config.add("areaFontSize", 26, "The size of the font of Area Display.", nil, {
 	data = {min = 1, max = 128},
 	category = "areaPlugin"
@@ -271,8 +274,8 @@ else
 
 	function PLUGIN:HUDPaint()
 		-- values
-		if (dieTrigger and dieTimer < RealTime() and dieAlpha <= 1) then
-			return	
+		if ((SURPRESS_AREADISPLAY == true) or (dieTrigger and dieTimer < RealTime() and dieAlpha <= 1)) then
+			return	 
 		end
 		
 		local w, h = ScrW(), ScrH()
