e1775f160438cfc441fdcb6461621196f6064d05
Kyu Yeon Lee <rebel1324@gmail.com>
2014-12-13 10:49:28 +0900

    Removing C Menu's other stuffs but Tool Menu.

---
+++ gamemode/core/derma/cl_contextmenu.lua
@@ -0,0 +1,206 @@
+
+local PANEL = {}
+
+AccessorFunc( PANEL, "m_bHangOpen", "HangOpen" )
+
+
+function PANEL:Init()
+	self:SetWorldClicker( true )
+
+	self.Canvas = vgui.Create( "DCategoryList", self )
+	self.m_bHangOpen = false
+end
+
+
+function PANEL:Open()
+
+	self:SetHangOpen( false )
+	
+	if ( g_SpawnMenu:IsVisible() ) then
+		g_SpawnMenu:Close( true )
+	end
+	
+	if ( self:IsVisible() ) then return end
+	
+	CloseDermaMenus()
+	
+	self:MakePopup()
+	self:SetVisible( true )
+	self:SetKeyboardInputEnabled( false )
+	self:SetMouseInputEnabled( true )
+	
+	RestoreCursorPosition()
+
+	local bShouldShow = true;
+
+	if ( bShouldShow && IsValid( spawnmenu.ActiveControlPanel() ) ) then
+		self.OldParent = spawnmenu.ActiveControlPanel():GetParent()
+		self.OldPosX, self.OldPosY = spawnmenu.ActiveControlPanel():GetPos()
+		spawnmenu.ActiveControlPanel():SetParent( self )
+		self.Canvas:Clear()
+		self.Canvas:AddItem( spawnmenu.ActiveControlPanel() )
+		self.Canvas:Rebuild()
+		self.Canvas:SetVisible( true )
+	else
+		self.Canvas:SetVisible( false )
+	end
+	
+	self:InvalidateLayout( true )
+
+end
+
+
+function PANEL:Close( bSkipAnim )
+
+	if ( self:GetHangOpen() ) then
+		self:SetHangOpen( false )
+		return
+	end
+	
+	RememberCursorPosition()
+	
+	CloseDermaMenus()
+
+	self:SetKeyboardInputEnabled( false )
+	self:SetMouseInputEnabled( false )
+	
+	self:SetAlpha( 255 )
+	self:SetVisible( false )
+	self:RestoreControlPanel()
+	
+end
+
+
+function PANEL:PerformLayout()
+	
+	self:SetPos( 0, 0 )
+	self:SetSize( ScrW(), ScrH() )
+
+	self.Canvas:SetWide( 311 )
+	self.Canvas:SetPos( ScrW() - self.Canvas:GetWide() - 50, self.y )
+	
+	if ( IsValid( spawnmenu.ActiveControlPanel() ) ) then
+	
+		spawnmenu.ActiveControlPanel():InvalidateLayout( true )
+		
+		local Tall = spawnmenu.ActiveControlPanel():GetTall() + 10
+		local MaxTall = ScrH() * 0.8
+		if ( Tall > MaxTall ) then Tall = MaxTall end
+		
+		self.Canvas:SetTall( Tall )
+		self.Canvas.y = ScrH() - 50 - Tall
+	
+	end
+	
+	self.Canvas:InvalidateLayout( true )
+	
+end
+
+
+function PANEL:StartKeyFocus( pPanel )
+
+	self:SetKeyboardInputEnabled( true )
+	self:SetHangOpen( true )
+	
+end
+
+
+function PANEL:EndKeyFocus( pPanel )
+
+	self:SetKeyboardInputEnabled( false )
+
+end
+
+
+function PANEL:RestoreControlPanel()
+	if ( !spawnmenu.ActiveControlPanel() ) then return end
+	if ( !self.OldParent ) then return end
+	
+	spawnmenu.ActiveControlPanel():SetParent( self.OldParent )
+	spawnmenu.ActiveControlPanel():SetPos( self.OldPosX, self.OldPosY )
+	
+	self.OldParent = nil
+end
+
+vgui.Register( "ContextMenu", PANEL, "EditablePanel" )
+
+function CreateContextMenu()
+	if ( IsValid( g_ContextMenu ) ) then
+		g_ContextMenu:Remove()
+		g_ContextMenu = nil
+	end
+
+	g_ContextMenu = vgui.Create( "ContextMenu" )
+	g_ContextMenu:SetVisible( false )
+	
+	g_ContextMenu.OnMousePressed = function( p, code )
+		hook.Run( "GUIMousePressed", code, gui.ScreenToVector( gui.MousePos() ) )
+	end
+	g_ContextMenu.OnMouseReleased = function( p, code )
+		hook.Run( "GUIMouseReleased", code, gui.ScreenToVector( gui.MousePos() ) )
+	end
+	
+	hook.Run( "ContextMenuCreated", g_ContextMenu )
+end
+
+function GM:OnContextMenuOpen()
+	if ( !hook.Call( "ContextMenuOpen", GAMEMODE ) ) then return end
+		
+	if ( IsValid( g_ContextMenu ) && !g_ContextMenu:IsVisible() ) then
+		g_ContextMenu:Open()
+		menubar.ParentTo( g_ContextMenu )
+	end
+end
+
+function GM:OnContextMenuClose()
+	if ( IsValid( g_ContextMenu ) ) then
+		g_ContextMenu:Close()
+	end
+end
+--[[   _                                
+    ( )                               
+   _| |   __   _ __   ___ ___     _ _ 
+ /'_` | /'__`\( '__)/' _ ` _ `\ /'_` )
+( (_| |(  ___/| |   | ( ) ( ) |( (_| |
+`\__,_)`\____)(_)   (_) (_) (_)`\__,_) 
+
+	DPanel
+
+--]]
+local PANEL = {}
+
+AccessorFunc( PANEL, "m_bBackground", 			"PaintBackground",	FORCE_BOOL )
+AccessorFunc( PANEL, "m_bBackground", 			"DrawBackground", 	FORCE_BOOL )
+AccessorFunc( PANEL, "m_bIsMenuComponent", 		"IsMenu", 			FORCE_BOOL )
+
+AccessorFunc( PANEL, "m_bDisabled", 	"Disabled" )
+AccessorFunc( PANEL, "m_bgColor", 		"BackgroundColor" )
+
+Derma_Hook( PANEL, "Paint", "Paint", "MenuBar" )
+
+--[[---------------------------------------------------------
+	
+-----------------------------------------------------------]]
+function PANEL:Init()
+end
+
+function PANEL:GetOpenMenu()
+end
+
+function PANEL:AddOrGetMenu( label )
+end
+
+function PANEL:AddMenu( label )
+end
+
+function PANEL:OnRemove()
+end
+
+--[[---------------------------------------------------------
+   Name: GenerateExample
+-----------------------------------------------------------]]
+function PANEL:GenerateExample( ClassName, PropertySheet, Width, Height )
+end
+
+
+derma.DefineControl( "DMenuBar", "", PANEL, "DPanel" )
