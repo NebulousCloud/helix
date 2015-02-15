c58630b1c6d92ebb940ce98ad674f78d604099dc
Chessnut <chessnutist@gmail.com>
2015-02-15 09:25:19 -0800

    Added entity:isChair() to check for chairs

--- gamemode/core/sh_util.lua
+++ gamemode/core/sh_util.lua
@@ -300,6 +300,22 @@ do
 		return self:GetClass():find("door")
 	end
 
+	-- Make a cache of chairs on start.
+	local CHAIR_CACHE = {}
+
+	-- Add chair models to the cache by checking if its vehicle category is a class.
+	for k, v in pairs(list.Get("Vehicles")) do
+		if (v.Category == "Chairs") then
+			CHAIR_CACHE[v.Model] = true
+		end
+	end
+
+	-- Whether or not a vehicle is a chair by checking its model with the chair list.
+	function entityMeta:isChair()
+		-- Micro-optimization in-case this gets used a lot.
+		return CHAIR_CACHE[self.GetModel(self)]
+	end
+
 	if (SERVER) then
 		-- Returns the door's slave entity.
 		function entityMeta:getDoorPartner()
