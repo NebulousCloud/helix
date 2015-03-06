8153394740677644b3f89d8f4516a14ad67a75fb
Chessnut <chessnutist@gmail.com>
2015-03-05 18:26:24 -0800

    Changed the way items handle getting data

--- gamemode/core/meta/sh_item.lua
+++ gamemode/core/meta/sh_item.lua
@@ -136,23 +136,28 @@ function ITEM:getData(key, default)
 
 		local value = self.data[key]
 
-		if (value == nil) then
-			if (IsValid(self.entity)) then
-				local data = self.entity:getNetVar("data", {})
-				local value = data[key]
-
-				if (value != nil) then
-					return value
-				end
-			end
-
-			return default
-		else
+		if (value != nil) then
 			return value
+		elseif (IsValid(self.entity)) then
+			local data = self.entity:getNetVar("data", {})
+			local value = data[key]
+
+			if (value != nil) then
+				return value
+			end
 		end
 	else
 		self.data = {}
+	end
+
+	if (default != nil) then
 		return default
+	end
+
+	local itemTable = nut.item.list[self.uniqueID]
+
+	if (itemTable and itemTable.data) then
+		return itemTable.data[key]
 	end
 end
 
