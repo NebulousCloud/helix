dac043dbadc4c5a1c95b3a8465ff40e060d2120d
Brian <chessnutist@gmail.com>
2015-06-30 17:44:38 +0700

    Added type validation for setting net vars
    Should probably add this to character data as well

--- gamemode/core/libs/sv_networking.lua
+++ gamemode/core/libs/sv_networking.lua
@@ -4,7 +4,26 @@ local playerMeta = FindMetaTable("Player")
 nut.net = nut.net or {}
 nut.net.globals = nut.net.globals or {}
 
+-- Check if there is an attempt to send a function. Can't send those.
+local function checkBadType(name, object)
+	local objectType = type(object)
+	
+	if (objectType == "function") then
+		ErrorNoHalt("Net var '"..name.."' contains a bad object type!")
+		
+		return true
+	elseif (objectType == "table") then
+		for k, v in pairs(object) do
+			-- Check both the key and the value for tables, and has recursion.
+			if (checkBadType(name, k) or checkBadType(name, v)) then
+				return true
+			end
+		end
+	end
+end
+
 function setNetVar(key, value, receiver)
+	if (checkBadType(key, value)) then return end
 	if (getNetVar(key) == value) then return end
 
 	nut.net.globals[key] = value
@@ -35,6 +54,8 @@ function entityMeta:clearNetVars(receiver)
 end
 
 function entityMeta:setNetVar(key, value, receiver)
+	if (checkBadType(key, value)) then return end
+		
 	nut.net[self] = nut.net[self] or {}
 
 	if (nut.net[self][key] != value) then
@@ -53,6 +74,8 @@ function entityMeta:getNetVar(key, default)
 end
 
 function playerMeta:setLocalVar(key, value)
+	if (checkBadType(key, value)) then return end
+	
 	nut.net[self] = nut.net[self] or {}
 	nut.net[self][key] = value
 
