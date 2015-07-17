fb5fea8aeac7f7d97b7196ac6fff92c10fb51beb
Chessnut <chessnutist@gmail.com>
2015-07-16 18:48:34 -0700

    Removed old license notices

--- plugins/storage/sh_definitions.lua
+++ plugins/storage/sh_definitions.lua
@@ -1,19 +1,4 @@
 --[[
-	NutScript is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	NutScript is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with NutScript.  If not, see <http://www.gnu.org/licenses/>.
---]]
-
-/*
 	PLUGIN.definitions[model's name(lowercase)] = {
 		name = "Crate",
 		desc = "A simple wooden create.",
@@ -22,7 +7,7 @@
 		locksound = "",
 		opensound = "",
 	}
-*/
+--]]
 
 PLUGIN.definitions["models/props_junk/wood_crate001a.mdl"] = {
 	name = "Crate",
