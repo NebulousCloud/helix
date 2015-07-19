f72bdf3d2445e8b924ab1a1fc890018affcfaadd
Chessnut <chessnutist@gmail.com>
2015-07-18 20:01:06 -0700

    Added nut.util.getTimeString()
    #s = seconds
    #m = minutes
    #h = hours
    #d = days
    #w = weeks
    #mo = months
    #y = years

--- gamemode/core/sh_util.lua
+++ gamemode/core/sh_util.lua
@@ -932,4 +932,39 @@ do
 
 		return os.difftime(os.time(date), os.time(localDate))
 	end
+
+	-- Setup for time strings.
+	local TIME_UNITS = {}
+	TIME_UNITS["s"] = 1						-- Seconds
+	TIME_UNITS["m"] = 60					-- Minutes
+	TIME_UNITS["h"] = 3600					-- Hours
+	TIME_UNITS["d"] = TIME_UNITS["h"] * 24	-- Days
+	TIME_UNITS["w"] = TIME_UNITS["d"] * 7	-- Weeks
+	TIME_UNITS["mo"] = TIME_UNITS["d"] * 30	-- Months
+	TIME_UNITS["y"] = TIME_UNITS["d"] * 365	-- Years
+
+	-- Gets the amount of seconds from a given formatted string.
+	-- Example: 5y2d7w = 5 years, 2 days, and 7 weeks.
+	-- If just given a minute, it is assumed minutes.
+	function nut.util.getStringTime(text)
+		local minutes = tonumber(text)
+
+		if (minutes) then
+			return math.abs(minutes * 60)
+		end
+
+		local time = 0
+
+		for amount, unit in text:lower():gmatch("(%d+)(%a+)") do
+			amount = tonumber(amount)
+			
+			if (amount and TIME_UNITS[unit]) then
+				time = time + math.abs(amount * TIME_UNITS[unit])
+			end
+		end
+
+		return time
+	end
+
+	print(nut.util.getStringTime("5"))
 end
