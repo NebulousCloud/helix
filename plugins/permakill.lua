80ef489ea724173d40eb4a642160454696b54057
Chessnut <chessnutist@gmail.com>
2015-07-20 10:38:10 -0700

    Fixed logic error in permakill plugin. Closes #304

--- plugins/permakill.lua
+++ plugins/permakill.lua
@@ -1,19 +1,23 @@
 PLUGIN.name = "Permakill"
 PLUGIN.author = "Thadah Denyse"
-PLUGIN.desc = "Adds permanent death in the server options"
+PLUGIN.desc = "Adds permanent death in the server options."
 
 function PLUGIN:PlayerDeath(client, inflictor, attacker)
 	local character = client:getChar()
 
-	if nut.config.get("pkActive") then
+	if (nut.config.get("pkActive")) then
+		if (nut.config.get("pkWorld") and (client == attacker or inflictor:IsWorld())) then
+			return
+		end
+
 		character:setData("permakilled", true)
-		if !nut.config.get("pkWorld") and ((client == attacker) or inflictor:IsWorld()) then return end
 	end
 end
 
 function PLUGIN:PlayerSpawn(client)
 	local character = client:getChar()
-	if (nut.config.get("pkActive") and character and character:getData("permakilled") == true) then
+
+	if (nut.config.get("pkActive") and character and character:getData("permakilled")) then
 		character:ban()
 	end
 end
@@ -23,6 +27,6 @@ nut.config.add("pkActive", false, "Whether or not permakill is activated on the 
 	category = "Permakill"
 })
 
-nut.config.add("pkWorld", false, "Whether or not world and self damage produce permanent death", nil, {
+nut.config.add("pkWorld", false, "Whether or not world and self damage produce permanent death.", nil, {
 	category = "Permakill"
 })
