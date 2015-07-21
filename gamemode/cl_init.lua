15b01ee868838955c8d9f6bfcc0f36b2f8b94454
Chessnut <chessnutist@gmail.com>
2015-07-21 14:01:02 -0700

    Fixed Sandbox hints showing. Closes #561

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -7,4 +7,9 @@ nut = nut or {util = {}, gui = {}, meta = {}}
 include("core/sh_util.lua")
 include("shared.lua")
 
+-- Sandbox stuff
 CreateConVar("cl_weaponcolor", "0.30 1.80 2.10", {FCVAR_ARCHIVE, FCVAR_USERINFO, FCVAR_DONTRECORD}, "The value is a Vector - so between 0-1 - not between 0-255")
+
+timer.Remove("HintSystem_OpeningMenu")
+timer.Remove("HintSystem_Annoy1")
+timer.Remove("HintSystem_Annoy2")
