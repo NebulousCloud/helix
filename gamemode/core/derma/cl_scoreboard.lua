f0e6acec2ba0b8060accf0188518e8609c23864c
SleepyMode <SleepyMode@users.noreply.github.com>
2017-06-17 16:11:38 +0300

    Update cl_scoreboard.lua

--- gamemode/core/derma/cl_scoreboard.lua
+++ gamemode/core/derma/cl_scoreboard.lua
@@ -81,7 +81,7 @@ local PANEL = {}
 
 	function PANEL:Think()
 		if ((self.nextUpdate or 0) < CurTime()) then
-			self.title:SetText(nut.config.get("scoreboard_title", GetConVarString("hostname")))
+			self.title:SetText(nut.config.get("sbTitle", GetConVarString("hostname")))
 
 			local visible, amount
 
