8715c7cc9aacd4cb2eaaebe4467df3d5d6a82ffc
Alexander Grist-Hucker <gristhavoc@gmail.com>
2018-02-19 19:28:02 +0000

    Prevented items from using persistence when the Save Items plugin is enabled.

--- plugins/saveitems.lua
+++ plugins/saveitems.lua
@@ -17,6 +17,12 @@ PLUGIN.description = "Saves items that were dropped."
 
 -- as title says.
 
+function PLUGIN:CanProperty(client, property, entity)
+	if (entity:GetClass() == "ix_item" and property == "persist") then
+		return false
+	end
+end
+
 function PLUGIN:LoadData()
 	local items = self:GetData()
 
