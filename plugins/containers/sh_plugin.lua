b5b57d6ddaf23928ce235ab8b3c310fa07bfdb62
Taxin2012 <mrtaxin2012@mail.ru>
2018-12-21 23:21:48 +0300

    Fix for containers
    For bug: Containers are saving with index 0 if callback not answer in time.

--- plugins/containers/sh_plugin.lua
+++ plugins/containers/sh_plugin.lua
@@ -46,11 +46,10 @@ if (SERVER) then
 
 				if (IsValid(container)) then
 					container:SetInventory(inventory)
+					self:SaveContainer()
+					entity:Remove()
 				end
 			end)
-
-			self:SaveContainer()
-			entity:Remove()
 		end
 	end
 
