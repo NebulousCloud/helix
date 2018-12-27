0f2ba7a79ca3920e7539f5f40278042d18a8e138
Igor Radovanovic <impulsh@gmail.com>
2018-12-27 16:59:48 -0500

    Remove old character last_join_time update

--- gamemode/core/libs/sh_character.lua
+++ gamemode/core/libs/sh_character.lua
@@ -136,11 +136,6 @@ if (SERVER) then
 							[1] = -1,
 						}
 
-						local timeQuery = mysql:Update("ix_characters")
-							timeQuery:Where("id", charID)
-							timeQuery:Update("last_join_time", math.floor(os.time()))
-						timeQuery:Execute()
-
 						local invQuery = mysql:Select("ix_inventories")
 							invQuery:Select("inventory_id")
 							invQuery:Select("inventory_type")
