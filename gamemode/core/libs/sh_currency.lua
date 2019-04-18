e8f1e98cda88e7e3bdd4dff1a4925d3f9a87272a
SleepyMode <sleepy@sleepymode.me>
2019-04-18 18:43:56 +0300

    Document currency library (#93)
    * Document currency library
    
    * Fix LDoc errors in currency library

--- gamemode/core/libs/sh_currency.lua
+++ gamemode/core/libs/sh_currency.lua
@@ -1,14 +1,27 @@
+
+--- A library representing the server's currency system.
+-- @module ix.currency
+
 ix.currency = ix.currency or {}
 ix.currency.symbol = ix.currency.symbol or "$"
 ix.currency.singular = ix.currency.singular or "dollar"
 ix.currency.plural = ix.currency.plural or "dollars"
 
+--- Sets the currency type.
+-- @realm shared
+-- @string symbol The symbol of the currency.
+-- @string singular The name of the currency in it's singular form.
+-- @string plural The name of the currency in it's plural form.
 function ix.currency.Set(symbol, singular, plural)
 	ix.currency.symbol = symbol
 	ix.currency.singular = singular
 	ix.currency.plural = plural
 end
 
+--- Returns a formatted string according to the current currency.
+-- @realm shared
+-- @number amount The amount of cash being formatted.
+-- @treturn string The formatted string.
 function ix.currency.Get(amount)
 	if (amount == 1) then
 		return ix.currency.symbol.."1 "..ix.currency.singular
@@ -17,6 +30,12 @@ function ix.currency.Get(amount)
 	end
 end
 
+--- Spawns an amount of cash at a specific location on the map.
+-- @realm shared
+-- @param[type=vector] pos The position of the money to be spawned.
+-- @number amount The amount of cash being spawned.
+-- @param[type=angle,opt=Angle(0,0,0)] angle The angle of the entity being spawned.
+-- @treturn entity The spawned money entity.
 function ix.currency.Spawn(pos, amount, angle)
 	if (!amount or amount < 0) then
 		print("[Helix] Can't create currency entity: Invalid Amount of money")
