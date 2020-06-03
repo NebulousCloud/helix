a6d41b3c499681833ce01f3b57d75159e9e80136
Gary Tate <me@garytate.co.uk>
2020-06-03 18:29:55 +0100

    Fixed yell range of recognition (#184)
    * Fixed number of bits to allow yell recognition
    
    2 bits only allowed for 0-3 when recognition was sending 4, causing it to never work.
    
    * Save a bit

--- plugins/recognition.lua
+++ plugins/recognition.lua
@@ -111,16 +111,16 @@ if (CLIENT) then
 	net.Receive("ixRecognizeMenu", function(length)
 		local menu = DermaMenu()
 			menu:AddOption(L"rgnLookingAt", function()
-				Recognize(1)
+				Recognize(0)
 			end)
 			menu:AddOption(L"rgnWhisper", function()
-				Recognize(2)
+				Recognize(1)
 			end)
 			menu:AddOption(L"rgnTalk", function()
-				Recognize(3)
+				Recognize(2)
 			end)
 			menu:AddOption(L"rgnYell", function()
-				Recognize(4)
+				Recognize(3)
 			end)
 		menu:Open()
 		menu:MakePopup()
@@ -152,7 +152,7 @@ else
 		if (isnumber(level)) then
 			local targets = {}
 
-			if (level < 2) then
+			if (level < 1) then
 				local entity = client:GetEyeTraceNoCursor().Entity
 
 				if (IsValid(entity) and entity:IsPlayer() and entity:GetCharacter()
@@ -162,9 +162,9 @@ else
 			else
 				local class = "w"
 
-				if (level == 3) then
+				if (level == 2) then
 					class = "ic"
-				elseif (level == 4) then
+				elseif (level == 3) then
 					class = "y"
 				end
 
