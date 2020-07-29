4e09b7ccaea47878466d99020b4076f7a87e6628
Sam Nyashev <12237569+Heyter@users.noreply.github.com>
2020-07-29 20:47:44 +0300

    Whether or not the item can fit in the rectangle of this inventory.

--- gamemode/core/meta/sh_inventory.lua
+++ gamemode/core/meta/sh_inventory.lua
@@ -569,12 +569,20 @@ if (SERVER) then
 			return false, "invalidItem"
 		end
 
+        if (hook.Run("CanTransferItem", item, targetInv) == false) then
+            return false, "notAllowed"
+        end
+
+        if (!x and y) or (x < 1 or y < 1 or x + item.width - 1 > self.w or y + item.height - 1 > self.h) then
+            x, y = self:FindEmptySlot(item.width, item.height)
+        end
+
+        if !(x and y) then
+            return false, "noFit"
+        end
+
 		if (isnumber(uniqueID)) then
 			local oldInvID = item.invID
-
-			if (!x and !y) then
-				x, y, bagInv = self:FindEmptySlot(item.width, item.height)
-			end
 
 			if (bagInv) then
 				targetInv = bagInv
@@ -586,105 +594,85 @@ if (SERVER) then
 				return false, "itemOwned"
 			end
 
-			if (hook.Run("CanTransferItem", item, ix.item.inventories[0], targetInv) == false) then
-				return false, "notAllowed"
-			end
+            targetInv.slots[x] = targetInv.slots[x] or {}
+            targetInv.slots[x][y] = true
 
-			if (x and y) then
-				targetInv.slots[x] = targetInv.slots[x] or {}
-				targetInv.slots[x][y] = true
+            item.gridX = x
+            item.gridY = y
+            item.invID = targetInv:GetID()
 
-				item.gridX = x
-				item.gridY = y
-				item.invID = targetInv:GetID()
+            for x2 = 0, item.width - 1 do
+                local index = x + x2
 
-				for x2 = 0, item.width - 1 do
-					local index = x + x2
+                for y2 = 0, item.height - 1 do
+                    targetInv.slots[index] = targetInv.slots[index] or {}
+                    targetInv.slots[index][y + y2] = item
+                end
+            end
 
-					for y2 = 0, item.height - 1 do
-						targetInv.slots[index] = targetInv.slots[index] or {}
-						targetInv.slots[index][y + y2] = item
-					end
-				end
+            if (!noReplication) then
+                targetInv:SendSlot(x, y, item)
+            end
 
-				if (!noReplication) then
-					targetInv:SendSlot(x, y, item)
-				end
+            if (!self.noSave) then
+                local query = mysql:Update("ix_items")
+                    query:Update("inventory_id", targetInv:GetID())
+                    query:Update("x", x)
+                    query:Update("y", y)
+                    query:Where("item_id", item.id)
+                query:Execute()
+            end
 
-				if (!self.noSave) then
-					local query = mysql:Update("ix_items")
-						query:Update("inventory_id", targetInv:GetID())
-						query:Update("x", x)
-						query:Update("y", y)
-						query:Where("item_id", item.id)
-					query:Execute()
-				end
+            hook.Run("InventoryItemAdded", ix.item.inventories[oldInvID], targetInv, item)
 
-				hook.Run("InventoryItemAdded", ix.item.inventories[oldInvID], targetInv, item)
-
-				return x, y, targetInv:GetID()
-			else
-				return false, "noFit"
-			end
+            return x, y, targetInv:GetID()
 		else
-			if (!x and !y) then
-				x, y, bagInv = self:FindEmptySlot(item.width, item.height)
-			end
-
 			if (bagInv) then
 				targetInv = bagInv
 			end
 
-			if (hook.Run("CanTransferItem", item, ix.item.inventories[0], targetInv) == false) then
-				return false, "notAllowed"
-			end
+            for x2 = 0, item.width - 1 do
+                local index = x + x2
 
-			if (x and y) then
-				for x2 = 0, item.width - 1 do
-					local index = x + x2
+                for y2 = 0, item.height - 1 do
+                    targetInv.slots[index] = targetInv.slots[index] or {}
+                    targetInv.slots[index][y + y2] = true
+                end
+            end
 
-					for y2 = 0, item.height - 1 do
-						targetInv.slots[index] = targetInv.slots[index] or {}
-						targetInv.slots[index][y + y2] = true
-					end
-				end
+            local characterID
+            local playerID
 
-				local characterID
-				local playerID
+            if (self.owner) then
+                local character = ix.char.loaded[self.owner]
 
-				if (self.owner) then
-					local character = ix.char.loaded[self.owner]
+                if (character) then
+                    characterID = character.id
+                    playerID = character.steamID
+                end
+            end
 
-					if (character) then
-						characterID = character.id
-						playerID = character.steamID
-					end
-				end
+            ix.item.Instance(targetInv:GetID(), uniqueID, data, x, y, function(newItem)
+                newItem.gridX = x
+                newItem.gridY = y
 
-				ix.item.Instance(targetInv:GetID(), uniqueID, data, x, y, function(newItem)
-					newItem.gridX = x
-					newItem.gridY = y
+                for x2 = 0, newItem.width - 1 do
+                    local index = x + x2
 
-					for x2 = 0, newItem.width - 1 do
-						local index = x + x2
+                    for y2 = 0, newItem.height - 1 do
+                        targetInv.slots[index] = targetInv.slots[index] or {}
+                        targetInv.slots[index][y + y2] = newItem
+                    end
+                end
 
-						for y2 = 0, newItem.height - 1 do
-							targetInv.slots[index] = targetInv.slots[index] or {}
-							targetInv.slots[index][y + y2] = newItem
-						end
-					end
+                if (!noReplication) then
+                    targetInv:SendSlot(x, y, newItem)
+                end
 
-					if (!noReplication) then
-						targetInv:SendSlot(x, y, newItem)
-					end
+                hook.Run("InventoryItemAdded", nil, targetInv, newItem)
+            end, characterID, playerID)
 
-					hook.Run("InventoryItemAdded", nil, targetInv, newItem)
-				end, characterID, playerID)
-
-				return x, y, targetInv:GetID()
-			else
-				return false, "noFit"
-			end
+            return x, y, targetInv:GetID()
 		end
 	end
 
