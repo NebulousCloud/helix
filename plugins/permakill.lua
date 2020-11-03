a223a59e89e2862ce19b7ac0ac7b2a32ee28813d
Wolfun Garden <56447563+WolfunGarden@users.noreply.github.com>
2020-11-03 01:44:20 +0100

    Update permakill.lua

--- plugins/permakill.lua
+++ plugins/permakill.lua
@@ -6,15 +6,17 @@ ix.config.Add("permakill", false, "Whether or not permakill is activated on the 
 	category = "Permakill"
 })
 
-ix.config.Add("permakillWorld", false, "Whether or not world and self damage produce permanent death.", nil, {
-	category = "Permakill"
-})
+function PLUGIN:PlayerShouldPermaKill(client, inflictor, attacker)
+	if !(ix.config.Get("permakillWorld") and (client == attacker or inflictor:IsWorld())) then
+		return false
+	end
+end
 
 function PLUGIN:PlayerDeath(client, inflictor, attacker)
 	local character = client:GetCharacter()
 
 	if (ix.config.Get("permakill")) then
-		if !(ix.config.Get("permakillWorld") and (client == attacker or inflictor:IsWorld())) then
+		if (hook.Run("PlayerShouldPermaKill", client, inflictor, attacker) == false) then
 			return
 		end
 		character:SetData("permakilled", true)
