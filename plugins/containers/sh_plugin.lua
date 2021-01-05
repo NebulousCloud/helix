6ed37a0a6d93f8dad0bfeada8ba1cae632dc109d
VindoesCompooter <100soundsystem32@gmail.com>
2021-01-05 13:37:43 +0000

    Fixed container exploit
    Fixed an exploit in both NutScript and Helix that allows a player to use Lua injection to brute force containers.

--- plugins/containers/sh_plugin.lua
+++ plugins/containers/sh_plugin.lua
@@ -17,6 +17,11 @@ ix.config.Add("containerOpenTime", 0.7, "How long it takes to open a container."
 	category = "Containers"
 })
 
+ix.config.Add("containerPasswordDelay", 1, "How long a user has to wait between password attempts.", nil, {
+	data = {min = 0, max = 50},
+	category = "Containers"
+})
+
 function ix.container.Register(model, data)
 	ix.container.stored[model] = data
 end
@@ -166,13 +171,18 @@ if (SERVER) then
 		local entity = net.ReadEntity()
 		local password = net.ReadString()
 		local dist = entity:GetPos():DistToSqr(client:GetPos())
-
-		if (dist < 16384 and password) then
-			if (entity.password and entity.password == password) then
-				entity:OpenInventory(client)
-			else
-				client:NotifyLocalized("wrongPassword")
+		
+		if (client.lastPasswordAttempt and CurTime() < client.lastPasswordAttempt + ix.config.Get("containerPasswordDelay",1)) then
+			client:NotifyLocalized("passwordTooQuick")
+		else
+			if (dist < 16384 and password) then
+				if (entity.password and entity.password == password) then
+					entity:OpenInventory(client)
+				else
+					client:NotifyLocalized("wrongPassword")
+				end
 			end
+			client.lastPasswordAttempt = CurTime()
 		end
 	end)
 
