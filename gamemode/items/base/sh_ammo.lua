277e2c014e88a1edae9a915339be0930e05e03b1
Alexander Grist-Hucker <alexgrist@users.noreply.github.com>
2021-04-10 00:38:50 +0100

    Use rounds item data for ammo items if set

--- gamemode/items/base/sh_ammo.lua
+++ gamemode/items/base/sh_ammo.lua
@@ -10,24 +10,28 @@ ITEM.category = "Ammunition"
 ITEM.useSound = "items/ammo_pickup.wav"
 
 function ITEM:GetDescription()
-	return Format(self.description, self.ammoAmount)
+	local rounds = self:GetData("rounds", self.ammoAmount)
+	return Format(self.description, rounds)
 end
 
 if (CLIENT) then
 	function ITEM:PaintOver(item, w, h)
 		draw.SimpleText(
-			item.ammoAmount, "DermaDefault",w - 5, h - 5, color_white, TEXT_ALIGN_RIGHT, TEXT_ALIGN_BOTTOM, 1, color_black
+			item:GetData("rounds", item.ammoAmount), "DermaDefault", w - 5, h - 5,
+			color_white, TEXT_ALIGN_RIGHT, TEXT_ALIGN_BOTTOM, 1, color_black
 		)
 	end
 end
 
 -- On player uneqipped the item, Removes a weapon from the player and keep the ammo in the item.
-ITEM.functions.use = { -- sorry, for name order.
+ITEM.functions.use = {
 	name = "Load",
 	tip = "useTip",
 	icon = "icon16/add.png",
 	OnRun = function(item)
-		item.player:GiveAmmo(item.ammoAmount, item.ammo)
+		local rounds = item:GetData("rounds", item.ammoAmount)
+
+		item.player:GiveAmmo(rounds, item.ammo)
 		item.player:EmitSound(item.useSound, 110)
 
 		return true
