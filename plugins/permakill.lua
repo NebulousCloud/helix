NebulousCloud/helix

commit 46d6a27380a9e32a4004dd613cec58ade5eae8ba

Fix permakill plugin (#275)

plugins/permakill.lua
@@ -13,10 +13,11 @@ ix.config.Add("permakillWorld", false, "Whether or not world and self damage pro
 function PLUGIN:PlayerDeath(client, inflictor, attacker)
 	local character = client:GetCharacter()
 
-	if (ix.config.Get("permakill")) then
-		if !(ix.config.Get("permakillWorld") and (client == attacker or inflictor:IsWorld())) then
+	if (ix.config.Get("permakill") and character) then
+		if (ix.config.Get("permakillWorld") and !(client == attacker or inflictor:IsWorld())) then
 			return
 		end
+
 		character:SetData("permakilled", true)
 	end
 end
@@ -25,5 +26,6 @@ function PLUGIN:PlayerSpawn(client)
 	local character = client:GetCharacter()
 	if (ix.config.Get("permakill") and character and character:GetData("permakilled")) then
 		character:Ban()
+		character:SetData("permakilled")
 	end
 end

commit 06bb1c576f1efabe17471310250c99f347a66408

Fix permakill not working with permakillWorld on
Seems that this was broken in #275

plugins/permakill.lua
@@ -19,7 +19,7 @@ function PLUGIN:PlayerDeath(client, inflictor, attacker)
 			return
 		end
 
-		if (ix.config.Get("permakillWorld") and !(client == attacker or inflictor:IsWorld())) then
+		if (ix.config.Get("permakillWorld") and (client == attacker or inflictor:IsWorld())) then
 			return
 		end
 