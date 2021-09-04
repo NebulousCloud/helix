44dee1bbdf90ec4896b4ebe4a8f9d1cf3a016c8c
LegAz <51002485+OffLegAz@users.noreply.github.com>
2021-09-04 14:56:17 +0300

    Return velocity check

--- plugins/stamina/sh_plugin.lua
+++ plugins/stamina/sh_plugin.lua
@@ -33,7 +33,7 @@ local function CalcStaminaChange(client)
 	local maxAttributes = ix.config.Get("maxAttributes", 100)
 	local offset
 
-	if (!client:GetNetVar("brth", false) and client:KeyDown(IN_SPEED)) then
+	if (!client:GetNetVar("brth", false) and client:KeyDown(IN_SPEED) and client:GetVelocity():LengthSqr() >= walkSpeed * walkSpeed) then
 		-- characters could have attribute values greater than max if the config was changed
 		offset = -ix.config.Get("staminaDrain", 1) + math.min(character:GetAttribute("end", 0), maxAttributes) / 100
 	else
