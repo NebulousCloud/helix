3e891a08a00b909f9850b6bf6d1c43baae2b9849
Gary Tate <me@garytate.co.uk>
2023-05-04 03:19:08 +0100

    Add empty table fallback to TableToJSON (#404)
    * Add empty table fallback to TableToJSON
    
    * fallback itemData earlier
    
    * Add back the table.Copy

--- gamemode/core/libs/sh_item.lua
+++ gamemode/core/libs/sh_item.lua
@@ -58,6 +58,8 @@ end
 
 function ix.item.Instance(index, uniqueID, itemData, x, y, callback, characterID, playerID)
 	if (!uniqueID or ix.item.list[uniqueID]) then
+		itemData = istable(itemData) and itemData or {}
+
 		local query = mysql:Insert("ix_items")
 			query:Insert("inventory_id", index)
 			query:Insert("unique_id", uniqueID)
@@ -77,7 +79,7 @@ function ix.item.Instance(index, uniqueID, itemData, x, y, callback, characterID
 				local item = ix.item.New(uniqueID, lastID)
 
 				if (item) then
-					item.data = istable(itemData) and table.Copy(itemData) or {}
+					item.data = table.Copy(itemData)
 					item.invID = index
 					item.characterID = characterID
 					item.playerID = playerID
