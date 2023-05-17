06e901dd571306068d1b279458d1945c101ad725
LegAz 2.0 <100089643+offlegaz2@users.noreply.github.com>
2023-05-17 23:04:00 +0300

    Whoops...

--- gamemode/core/libs/sh_character.lua
+++ gamemode/core/libs/sh_character.lua
@@ -1100,7 +1100,7 @@ do
 		-- Used for setting random access vars on the "var" character var (really stupid).
 		-- Clean this up someday.
 		net.Receive("ixCharacterVar", function()
-			local id = net.ReadUInt(14)
+			local id = net.ReadUInt(32)
 			local character = ix.char.loaded[id]
 
 			if (character) then
