dec8ecd22d9fd5db09dde71c777ec2230ddd8163
Fer3D <ferbg9@gmail.com>
2024-10-02 02:23:31 +0200

    Multilingual support for the 'Equip' and 'Unequip' options of items.

--- gamemode/items/base/sh_weapons.lua
+++ gamemode/items/base/sh_weapons.lua
@@ -75,7 +75,7 @@ end)
 
 -- On player uneqipped the item, Removes a weapon from the player and keep the ammo in the item.
 ITEM.functions.EquipUn = { -- sorry, for name order.
-	name = "Unequip",
+	name = "unequip",
 	tip = "equipTip",
 	icon = "icon16/cross.png",
 	OnRun = function(item)
@@ -92,7 +92,7 @@ ITEM.functions.EquipUn = { -- sorry, for name order.
 
 -- On player eqipped the item, Gives a weapon to player and load the ammo data from the item.
 ITEM.functions.Equip = {
-	name = "Equip",
+	name = "equip",
 	tip = "equipTip",
 	icon = "icon16/tick.png",
 	OnRun = function(item)
