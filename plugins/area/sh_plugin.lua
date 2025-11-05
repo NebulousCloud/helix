f9e84843fe542896b45cb91a88efdf3eb2be93eb
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2025-11-05 13:39:40 +0200

    refactor(areas): readjust timer instead of re-creating it

--- plugins/area/sh_plugin.lua
+++ plugins/area/sh_plugin.lua
@@ -13,10 +13,7 @@ ix.area.stored = ix.area.stored or {}
 ix.config.Add("areaTickTime", 1, "How many seconds between each time a character's current area is calculated.",
 	function(oldValue, newValue)
 		if (SERVER) then
-			timer.Remove("ixAreaThink")
-			timer.Create("ixAreaThink", newValue, 0, function()
-				PLUGIN:AreaThink()
-			end)
+			timer.Adjust("ixAreaThink", newValue)
 		end
 	end,
 	{
